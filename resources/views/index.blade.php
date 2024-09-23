4d172df8d2f6a148a12134c40c475673462a19a4
trungvuquoc <137154114+trungvuquoc@users.noreply.github.com>
2024-09-23 20:35:43 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,8 @@
            <div class="hero__slider">
               <div class="item-video">
                    <video width="100%" height="750px" controls>
-                       <source src="https://youtu.be/Z9BnH_tZPJo">
+                     {{--  <source src="https://youtu.be/Z9BnH_tZPJo"> --}}
+                     <source  "https://youtu.be/Z9BnH_tZPJo">
                         Your browser does not support the video tag.
                     </video>
                </div>
