351b664fcf3f13713aa3889a4458275b872f8da7
tungks <tungks@watermelon.vn>
2024-10-02 17:41:25 +0700

    fix

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-md-3">
+            <div class="col-lg-12 col-md-12 text-center">
                 <div class="header__logo">
                     <a href="/"><img src="/img/logo.png" style="height: 100px" alt=""></a>
                 </div>
